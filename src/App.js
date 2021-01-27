fb468a95deb0fb47d082b4946ca32db446c95c91
tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-01-27 21:07:32 +0700

    UseEffect&destroy listener

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Usestatedemo1 from './components/UseStateDemo1'
 import Usestatedemo2 from './components/UseStateDemo2'
 import UseStatedemo3 from './components/UseStateDemo3'
 import UseEffectdemo1 from './components/UseEffectdemo1'
+import UseEffectdemo2 from './components/UseEffectdemo2'
 
 export default function App() {
+  const [isShow, setIsShow] = React.useState(true)
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       {/* <Usestatedemo1/> */}
       {/* <Usestatedemo2/> */}
-      <UseStatedemo3/>
-      {/* <UseEffectdemo1> */}
+      {/* <UseStatedemo3/> */}
+      {/* <UseEffectdemo1/> */}
+     {isShow && <UseEffectdemo2/>}
+     <button onClick={()=>setIsShow(false)}>Hide</button>
     </div>
   )
 }
--- src/components/UseEffectdemo2.js
+++ src/components/UseEffectdemo2.js
@@ -1,14 +1,16 @@
 import React,{useState}  from 'react'
 
-export default function UseEffectdemo1() {
+export default function UseEffectdemo2() {
     const [count, setcount] = useState(0)
     React.useEffect(() => {
         //called whenever state is changed
         document.title= "current count ="+count;
-
+        return ()=>{
+            //called when component is deleted
+        document.title= "the component was just destroyed"
+        }
     }, [count])
     return (
-        
         <div>
             <h1>{count}</h1>
             <button onClick={()=>setcount(count +1)}>add</button>
